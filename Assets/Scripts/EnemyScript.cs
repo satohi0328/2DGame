5a81dace9b52fa46007cfd296cacee5385cbb2ca
satohi0328 <31176803+satohi0328@users.noreply.github.com>
2020-04-26 12:20:01 +0900

    Add newProject
    初回コミット

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    //プライベート変数
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
+    private bool isGround = false;
+    private bool isJump = false;
+    private bool isHead = false;
+    private float jumpPos = 0.0f;
+    private float beforeKey;
+    private float dashTime, jumpTime;
+    private float lapseTime = 0.0f; //攻撃後の経過時間
+    private bool isAttack = false; // 攻撃したかどうか
+    private float myDirection = -1f; //キャラクターの向き
+
+    //パブリック変数
+    public float speed; //速度
+    public float gravity; //重力
+    public float jumpSpeed;//ジャンプする速度
+    public float jumpHeight;//高さ制限
+    public float jumpLimitTime;//ジャンプ制限時間
+    public GroundCheck ground; //接地判定
+    public GroundCheck head;//頭ぶつけた判定
+    public AnimationCurve dashCurve; //
+    public AnimationCurve jumpCurve; //
+    public GameObject ibarst; // 攻撃オブジェクト
+    public float coolTime = 0.5f; //攻撃のクールタイム
+
+
+
+    //Playerの状態
+    public enum MyState {
+        Idle,
+        Attack,
+        Jum
+    };
+
+    // Use this for initialization
+    void Start() {
+        //コンポーネントのインスタンスを捕まえる
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        //lapseTimeを初期化
+        lapseTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        //接地判定
+        isGround = ground.IsGround();
+
+        //各種座標軸の速度を求める
+        float xSpeed = GetXSpeed();
+        float ySpeed = GetYSpeed();
+
+        // 攻撃できるか判定
+        if (JudgeAttack()) {
+            if (Input.GetKey(KeyCode.Space)) {
+                Ibarst();
+                anim.SetTrigger("attack_Trigger");
+            }
+
+        }
+
+        //移動速度を設定
+        rb.velocity = new Vector2(xSpeed, ySpeed);
+
+        //アニメーションを適用
+        SetAnimation();
+
+    }
+
+    /**
+     *モーションその１
+     * めっちゃ攻撃
+     */
+    private void Pattern1() {
+
+    }
+
+    /// <summary>
+    /// Y成分で必要な計算をし、速度を返す。
+    /// </summary>
+    /// <returns>Y軸の速さ</returns>
+    private float GetYSpeed() {
+        float verticalKey = Input.GetAxis("Vertical");
+        float ySpeed = -gravity;
+
+        if (isGround) {
+            if (verticalKey > 0 && jumpTime < jumpLimitTime) {
+                ySpeed = jumpSpeed;
+                jumpPos = transform.position.y; //ジャンプした位置を記録する
+                isJump = true;
+                jumpTime = 0.0f;
+            } else {
+                isJump = false;
+            }
+        } else if (isJump) {
+            //上ボタンを押されている。かつ、現在の高さがジャンプした位置から自分の決めた位置より下ならジャンプを継続する
+            if (verticalKey > 0 && jumpPos + jumpHeight > transform.position.y && jumpTime < jumpLimitTime && !isHead) {
+                ySpeed = jumpSpeed;
+                jumpTime += Time.deltaTime;
+            } else {
+                isJump = false;
+                jumpTime = 0.0f;
+            }
+        }
+
+        if (isJump) {
+            ySpeed *= jumpCurve.Evaluate(jumpTime);
+        }
+
+        return ySpeed;
+    }
+
+    /// <summary>
+    /// X成分で必要な計算をし、速度を返す。
+    /// </summary>
+    /// <returns>X軸の速さ</returns>
+    private float GetXSpeed() {
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+
+        if (horizontalKey > 0) {
+            transform.localScale = new Vector3(1, 1, 1);
+            dashTime += Time.deltaTime;
+            anim.SetBool("is_walk", true);
+        } else if (horizontalKey < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+            dashTime += Time.deltaTime;
+            anim.SetBool("is_walk", true);
+        } else {
+            anim.SetBool("is_walk", false);
+            xSpeed = 0.0f;
+            dashTime = 0.0f;
+        }
+
+        //前回の入力からダッシュの反転を判断して速度を変える
+        if (horizontalKey > 0 && beforeKey < 0) {
+            dashTime = 0.0f;
+        } else if (horizontalKey < 0 && beforeKey > 0) {
+            dashTime = 0.0f;
+        }
+
+        beforeKey = horizontalKey;
+        xSpeed *= dashCurve.Evaluate(dashTime);
+        beforeKey = horizontalKey;
+        return xSpeed * myDirection;
+    }
+
+    /// <summary>
+    /// アニメーションを設定する
+    /// </summary>
+    private void SetAnimation() {
+        anim.SetBool("jump", isJump);
+    }
+
+
+
+    private void Ibarst() {
+        // 攻撃オブジェクトを生成
+        GameObject g = Instantiate(ibarst);
+        Debug.Log(myDirection);
+        //右向きの場合
+        g.transform.localScale = new Vector2(g.transform.localScale.x * myDirection, g.transform.localScale.y);
+        g.transform.position = new Vector2(this.transform.position.x + 1f * myDirection, transform.position.y - 0.5f);
+        isAttack = true;
+    }
+
+    private bool JudgeAttack() {
+
+        // 攻撃モーション中の場合
+        if (isAttack) {
+            // クールタイム中の場合
+            if (coolTime > lapseTime) {
+                // 経過時間を加算して終了
+                lapseTime += Time.deltaTime;
+                return false;
+            } else {
+                // 変数初期化
+                isAttack = false;
+                lapseTime = 0;
+                return true;
+            }
+        }
+
+        return true;
+
+    }
+
+    //public void SetState(MyState tempState) {
+    //    if (tempState == MyState.Normal) {
+    //        state = MyState.Normal;
+    //    } else if (tempState == MyState.Attack) {
+    //        velocity = Vector3.zero;
+    //        state = MyState.Attack;
+    //        animator.SetTrigger("Attack");
+    //    }
+    //}
+}
---
+++ Assets/Scripts/IbarstScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IbarstScript : MonoBehaviour {
+
+	[Header("スピード")] public float speed = 5.0f;
+	[Header("最大移動距離")] public float maxDistance = 100.0f;
+
+	public GameObject explosion;
+
+	private Rigidbody2D rb;
+	private Vector3 defaultPos;
+
+	private float direction;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		if (rb == null) {
+			Debug.Log("設定が足りません");
+			Destroy(this.gameObject);
+		}
+
+		direction = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+        //      if(this.transform.position.x > Screen.width) {
+        //	Destroy(this.gameObject);
+        //      } else {
+        //	rb.MovePosition(transform.position += Vector3.left * Time.deltaTime * speed);
+        //}
+        rb.MovePosition(transform.position += Vector3.right * Time.deltaTime * speed * direction);
+
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+
+        // 攻撃オブジェクト同士で相殺しない
+        if (!collision.CompareTag("Ibarst")) {
+            Instantiate(explosion,transform.position,transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
